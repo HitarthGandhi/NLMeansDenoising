697aa035d85218c514e4dea81a053f23ae646f4a
varunjain3 <varun.jain@iitgn.ac.in>
2020-11-18 17:59:21 +0530

    added comments

--- NLmeans.py
+++ NLmeans.py
@@ -48,24 +48,8 @@ def get_window(img, x, y, N=25):
 
     return window
 
-# Provided by Matlab
-
-
-def matlab_style_gauss2d(shape=(3, 3), sigma=0.5):
-    """
-    2D gaussian mask - should give the same result as MATLAB's
-    fspecial('gaussian',[shape],[sigma])
-    """
-    m, n = [(ss-1.)/2. for ss in shape]
-    y, x = np.ogrid[-m:m+1, -n:n+1]
-    h = np.exp(-(x*x + y*y)/(2.*sigma*sigma))
-    h[h < np.finfo(h.dtype).eps*h.max()] = 0
-    sumh = h.sum()
-    if sumh != 0:
-        h /= sumh
-    return h
-
-def NL_means(img,h=8,f=4,t=11):
+# The main function
+def NL_means(img,h=8.5,f=4,t=11):
     # neighbourhood size 2f+1
     N = 2*f + 1
 
@@ -75,44 +59,62 @@ def NL_means(img,h=8,f=4,t=11):
     # Filtering Parameter
     sigma_h = h
 
+    # Padding the image
     pad_img = np.pad(img, t+f)
 
+    # Getting the height and width of the image
     h, w = img.shape
     h_pad, w_pad = pad_img.shape
 
     neigh_mat = np.zeros((h+S-1, w+S-1, N, N))
 
-
+    # Making a dp neighbourhood for all pixels (used for vectorizing sliding window algorithm)
     for y in range(h+S-1):
         for x in range(w+S-1):
             neigh_mat[y, x] = np.squeeze(get_window(
                 pad_img[:, :, np.newaxis], x+f, y+f, 2*f+1))
 
+    # Empty image to be filled by the algorithm
     output = np.zeros(img.shape)
 
-
+    # Initializing the counter
     prog = tqdm(total=(h-1)*(w-1), position=0, leave=True)
 
+    # Iterating for each pixel
     for Y in range(h):
         for X in range(w):
+            # Shifting for padding
             x = X + t
             y = Y + t
+            # Getting neibourhood in chunks of search window
             a = get_window(np.reshape(
                 neigh_mat, (h+S-1, w+S-1, N*N)), x, y, S)
+            
+            # Getting self Neigbourhood
             b = neigh_mat[y, x].flatten()
+
+            # Getting distance of vectorized neibourhood
             c = a-b
+
+            # Determining weights
             d = c*c
             e = np.sqrt(np.sum(d, axis=2))
             F = np.exp(-e/(sigma_h*sigma_h))
+
+            # Summing weights
             Z = np.sum(F)
+
+            # Calculating average pixel value
             im_part = np.squeeze(get_window(pad_img[:, :, None], x+f, y+f, S))
             NL = np.sum(F*im_part)
             output[Y, X] = NL/Z
+            
+            # Updating counter
             prog.update(1)
     return output
 
 def main():
-    
+    # If called using seprate entity.
     name = "Image3.png"
 
     img_path = "sp_noise\{}".format(name)
